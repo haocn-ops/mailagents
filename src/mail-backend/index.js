0ebabf20045d98f0c10d3f8b053a6947fb9bb9ee
zh <zh@zhs-MacBook-Pro.local>
2026-03-09 20:31:54 +0800

    Extract mail backend runtime config

--- src/mail-backend/index.js
+++ src/mail-backend/index.js
@@ -1,19 +1,10 @@
 import { MailuInternalAdapter } from "./mailu-internal-adapter.js";
 import { NoopMailBackend } from "./noop-backend.js";
+import { buildMailuAdapterConfig } from "./runtime-config.js";
 
 export function createMailBackendAdapter(runtimeConfig) {
   if (runtimeConfig.mailProvider === "mailu") {
-    return new MailuInternalAdapter({
-      mailboxDomain: runtimeConfig.mailboxDomain,
-      baseUrl: runtimeConfig.mailuBaseUrl,
-      apiToken: runtimeConfig.mailuApiToken,
-      releaseMode: runtimeConfig.mailuReleaseMode,
-      quotaBytes: runtimeConfig.mailuQuotaBytes,
-      authScheme: runtimeConfig.mailuAuthScheme,
-      smtpHost: runtimeConfig.mailSmtpHost,
-      smtpPort: runtimeConfig.mailSmtpPort,
-      smtpSecure: runtimeConfig.mailSmtpSecure,
-    });
+    return new MailuInternalAdapter(buildMailuAdapterConfig(runtimeConfig));
   }
 
   return new NoopMailBackend();
